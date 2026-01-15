0828be11c6cdd424f3ed7206340bb8698bf04419
Bigoni Luca <luca.bigoni@gmail.com>
2026-01-15 18:09:36 +0100

    Update agent service python script

--- service/agent_service.py
+++ service/agent_service.py
@@ -1,6 +1,24 @@
 import time
 import subprocess
+import sys
+import os
 
-while True:
-    subprocess.call(["python", "network_probe.py"])
-    time.sleep(60)
+BASE_DIR = os.path.dirname(sys.executable)
+
+EXTENSION_SCRIPT = os.path.join(
+    BASE_DIR,
+    "network_probe.py"
+)
+
+INTERVAL = 60
+
+def main():
+    while True:
+        try:
+            subprocess.call([sys.executable, EXTENSION_SCRIPT])
+        except Exception:
+            pass
+        time.sleep(INTERVAL)
+
+if __name__ == "__main__":
+    main()
